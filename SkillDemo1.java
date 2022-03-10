5c544360deebaa1e236c1e0202e64ef55ea18930
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-03-09 23:16:51 -0800

    create skilldemo1 and skilldemo1test
    test will fail

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,1 +1,5 @@
-public class SkillDemo1.java{} // write code in vs later
+public class SkillDemo1{
+    public static String birthday(int age){
+        return "happy " + age + "th birthday!";
+    }
+} // write code in vs later
